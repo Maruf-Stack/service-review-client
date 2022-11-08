8fef71f83b5110a0bf20fd706eb86138c3ca440c
Maruf <01780527702maruf@gmail.com>
2022-11-08 22:08:54 +0600

    banner text

--- src/pages/banner/Banner.js
+++ src/pages/banner/Banner.js
@@ -3,10 +3,11 @@ import './banner.css'
 const Banner = () => {
     return (
         <div className='banner'>
-            <div className='flex flex-col justify-center banner-title'>
+            <div className='px-12 banner-title py-52'>
 
-                <h1 className=''>Im a proffesional photographer</h1>
-                <p>Any kinds of ocation such as marriage ceremony,holud night,picnic.If your want to capture these moments is your album you need to capture these moments.Plaease contact with me for more details</p>
+                <h1 className='mb-4 font-serif text-5xl font-bold text-slate-900'>Im a proffesional <span className='text-blue-900 '>,,</span></h1>
+                <h1 className='mb-8 font-serif text-5xl font-bold text-slate-900'>Photographer</h1>
+                <p className='text-lg font-medium text-slate-500'>Any kinds of ocation such as marriage ceremony,holud night,picnic.<br></br>If your want to capture these moments is your album you need<br></br> to capture these moments.Plaease contact with me for<br></br>  more details</p>
 
             </div>
         </div>
