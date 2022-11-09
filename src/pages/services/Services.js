2e377448b72d6a22f9e097a1906d095d40041d5d
Maruf <01780527702maruf@gmail.com>
2022-11-09 13:26:49 +0600

    card add

--- src/pages/services/Services.js
+++ src/pages/services/Services.js
@@ -1,16 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import Service from '../service/Service';
 
 const Services = () => {
-
+    const [services, setServices] = useState([])
     useEffect(() => {
         const url = 'http://localhost:5000/services';
         fetch(url)
             .then(res => res.json())
-            .then(data => console.log(data))
+            .then(data => setServices(data))
     }, [])
     return (
-        <div>
-
+        <div className='grid grid-cols-3'>
+            {
+                services.map(service => <Service service={service} key={service._id}></Service>)
+            }
         </div>
     );
 };
